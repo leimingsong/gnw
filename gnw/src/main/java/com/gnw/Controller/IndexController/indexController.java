bf3f626299ffff022a71fcde28308e0b48b1fcbe
brilliantstz <brilliantstz@qq.com>
2019-11-19 18:40:59 +0800

    利用榛子云第三方实现短信验证2

--- gnw/src/main/java/com/gnw/Controller/IndexController/indexController.java
+++ gnw/src/main/java/com/gnw/Controller/IndexController/indexController.java
@@ -3,17 +3,24 @@ package com.gnw.Controller.IndexController;
 import com.gnw.Util.Result;
 import com.gnw.Service.indexService.IndexXXXService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class indexController {
     @Autowired
     IndexXXXService indexXXXService;
 
     @GetMapping("/index")
+    @ResponseBody
     public ModelAndView Index(){
         return new ModelAndView("/index");
+    }
+
+    @GetMapping("/toRegister")
+    public ModelAndView toRegister(){
+        return new ModelAndView("/client/register");
     }
 
 }
